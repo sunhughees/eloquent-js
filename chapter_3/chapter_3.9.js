20135c0044d9fd8c741af3fc56f8a8c590527844
sven-sun <sunhughees@sina.cn>
2016-03-24 21:29:18 +0800

    modified:   chapter_3/chapter_3.9.js

--- chapter_3/chapter_3.9.js
+++ chapter_3/chapter_3.9.js
@@ -1,3 +1,4 @@
+/*
 function power(base, exponent) {
 	if (exponent == 0)
 		return 1;
@@ -6,3 +7,19 @@ function power(base, exponent) {
 }
 
 console.log(power(2,3));
+*/
+
+function findSolution(target) {
+	function find(start, history) {
+		if (start == target)
+			return history;
+		else if (start > target)
+			return null;
+		else
+			return find(start + 5, "(" + history + " + 5)") ||
+				   find(start * 3, "(" + history + " * 3)");
+	}
+	return find(1, "1");
+}
+
+console.log(findSolution(24));
