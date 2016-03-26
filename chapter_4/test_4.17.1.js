2de44b201b903c2ed43a11adab7c3328d2b4835d
sunhughees <sunhughees@sina.cn>
2016-03-26 17:34:22 +0800

    modified:   test_4.17.1.js

--- chapter_4/test_4.17.1.js
+++ chapter_4/test_4.17.1.js
@@ -1,8 +1,23 @@
-function range(start, end) {
-    var list = [];
-    for (var i=start; i<=end; i++)
-        list.push(i);
-    return list;
+function range(start, end, interval) {
+        	
+    if (start<=end)
+	{
+        if (arguments.length==2)
+            interval=1;
+        var list = [];
+        for (var i=start; i<=end; i = i+interval)
+            list.push(i);
+        return list;
+	}
+    else
+    {
+        if (arguments.length==2)
+            interval=-1;
+        var list = [];
+        for (var i=end; i<=start; i = i-interval)
+            list.unshift(i);
+        return list; 
+    }	
 }
 
 function sum(list) {
@@ -13,6 +28,8 @@ function sum(list) {
     return result;
 }
 
+console.log(range(10,1,-2));
+console.log(range(10,1));
 console.log(range(1,10));
 console.log(sum(range(1,10)));
 
