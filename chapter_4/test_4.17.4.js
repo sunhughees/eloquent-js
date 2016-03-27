e9cdada9527d8ab092a671a7988814456899bcb5
sunhughees <sunhughees@sina.cn>
2016-03-27 22:21:43 +0800

    new file:   test_4.17.4.js

---
+++ chapter_4/test_4.17.4.js
@@ -0,0 +1,12 @@
+var object1 = {value:10};
+var object2 = {value:10};
+
+console.log(object1==object2);
+
+console.log({x:2}==={x:2});
+
+for (var x in object1)
+	console.log(x);
+function deepEqual(a,b) {
+
+}
