aa9c040f8cf85c1f6eb3c9b2efaf50d4ff8d07a9
sunhughees <sunhughees@sina.cn>
2016-03-27 09:29:17 +0800

    modified:   test_4.17.2.js

--- chapter_4/test_4.17.2.js
+++ chapter_4/test_4.17.2.js
@@ -5,10 +5,19 @@ function reverseArray(list) {
 	return newList;
 }
 
+function reverseArrayInPlace(list) {
+	var newList = reverseArray(list);
+	for (i=0; i<=list.length-1; i++)
+		list[i] = newList[i];
+	return list;
+}
+
 
 var aList = [1,2,3];
 
 console.log(reverseArray(aList));
 console.log(aList);
 
+console.log(reverseArrayInPlace(aList));
+console.log(aList);
 
