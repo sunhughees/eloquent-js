876ee1083da7d825f74723dea81cdb4e5b5ed032
sven-sun <sunhughees@sina.cn>
2016-04-04 18:50:27 +0800

    new file:   chapter_6/chapter_6.2.js

---
+++ chapter_6/chapter_6.2.js
@@ -0,0 +1,12 @@
+var rabbit = {};
+rabbit.speak = function(line) {
+	console.log("The " + this.type + "rabbit says '" + line + "'");
+};
+
+rabbit.speak("I'm alive.");
+
+var whiteRabbit = {type: "White", speak: rabbit.speak};
+
+whiteRabbit.speak("I'm alive.");
+
+
