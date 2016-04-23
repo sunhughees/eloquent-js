9945fed91890fc263a2e4603c9ba80024999daea
sunhughees <sunhughees@sina.cn>
2016-04-23 14:20:52 +0800

    add md

--- chapter_6/chapter_6.2.js
+++ chapter_6/chapter_6.2.js
@@ -1,6 +1,6 @@
 var rabbit = {};
 rabbit.speak = function(line) {
-	console.log("The " + this.type + "rabbit says '" + line + "'");
+	console.log("The " + this.type + " rabbit says '" + line + "'");
 };
 
 rabbit.speak("I'm alive.");
@@ -8,3 +8,10 @@ rabbit.speak("I'm alive.");
 var whiteRabbit = {type: "White", speak: rabbit.speak};
 
 whiteRabbit.speak("I'm alive.");
+
+// function speak(line) {
+// 	console.log(this.type);
+// }
+// speak();
+
+rabbit.speak.apply(whiteRabbit, ["Burp"]);
