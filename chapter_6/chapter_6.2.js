55d2fd2c4260a4fa7ba249e718be3c7463c49900
sunhughees <sunhughees@sina.cn>
2016-04-23 19:28:16 +0800

    mod two

--- chapter_6/chapter_6.2.js
+++ chapter_6/chapter_6.2.js
@@ -27,19 +27,12 @@ var protoRabbit = {
 	}
 };
 
-
-
 function Rabbit(type) {
 	this.type= type;
 };
 
-
-
 console.log([1,2].toString());
 console.log(Object.prototype.toString.call([1,2]));
-
-
-
 
 var killerRabbit = new Rabbit("killer");
 
@@ -54,3 +47,27 @@ Rabbit.prototype.dance = function() {
 killerRabbit.speak("SKREEEE!");
 
 killerRabbit.dance();
+
+var map = {};
+function storePhi(event, phi) {
+	map[event] = phi;
+};
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+
+Object.prototype.nonsense = "hi";
+
+for (var name in map)
+	console.log(name);
+
+delete Object.prototype.nonsense;
+
+for (var name in map)
+	console.log(name);
+
+//无原型对象（不存在默认定义的属性或方法）
+var map = Object.create(null);
+map["pizza"] = 0.069;
+console.log("toString" in map);
+console.log("pizza" in map);
