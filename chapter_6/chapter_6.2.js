c35ac7148141fa7a8e331e3d2faf6d23cbb913f3
sunhughees <sunhughees@sina.cn>
2016-04-23 17:11:56 +0800

    modified:   chapter_6/chapter_6.2.js

--- chapter_6/chapter_6.2.js
+++ chapter_6/chapter_6.2.js
@@ -15,3 +15,42 @@ whiteRabbit.speak("I'm alive.");
 // speak();
 
 rabbit.speak.apply(whiteRabbit, ["Burp"]);
+
+var empty = {};
+console.log(empty.toString);
+console.log(Object.prototype);
+console.log(Object.getPrototypeOf({}));
+
+var protoRabbit = {
+	speak: function(line) {
+		console.log("The " + this.type + " rabit says '" + line + "'");
+	}
+};
+
+
+
+function Rabbit(type) {
+	this.type= type;
+};
+
+
+
+console.log([1,2].toString());
+console.log(Object.prototype.toString.call([1,2]));
+
+
+
+
+var killerRabbit = new Rabbit("killer");
+
+Rabbit.prototype.speak = function(line) {
+	console.log("The " + this.type + " rabbit says '" + line + "'");
+};
+
+Rabbit.prototype.dance = function() {
+	console.log("The " + this.type + " rabbit dances a jig.");
+};
+
+killerRabbit.speak("SKREEEE!");
+
+killerRabbit.dance();
